9d30b108da5fb141b7696c1bb76e7ea077f91cf4
zheNeng <gpound.liu@cardinfolink.com>
2019-02-15 10:21:51 +0800

    改exclude

--- publicUtil/changeJsConfig.js
+++ publicUtil/changeJsConfig.js
@@ -5,16 +5,14 @@ function resolve(p) {
   const res = path.resolve(__dirname, p);
   return res;
 }
-fs.writeFileSync(
-  resolve("../jsconfig.json"),
-  JSON.stringify({
-    compilerOptions: {
-      baseUrl: resolve("../"),
-      paths: {
-        "@/*": [`src/${process.env.ENV_file}/*`],
-        "=_=/*": ["publicUtil/*"]
-      }
-    },
-    exclude: ["dist"]
-  })
-);
+const congig = {
+  compilerOptions: {
+    baseUrl: resolve("../"),
+    paths: {
+      "@/*": [`src/${process.env.ENV_file}/*`],
+      "=_=/*": ["publicUtil/*"]
+    }
+  },
+  exclude: ["dist", "node_modules", "bower_components"]
+};
+fs.writeFileSync(resolve("../jsconfig.json"), JSON.stringify(congig));

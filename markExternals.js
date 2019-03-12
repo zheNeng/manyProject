13bd76474dc3030175a8699cf18aaab8eacc8c93
zheNeng <gpound.liu@cardinfolink.com>
2019-03-12 09:47:40 +0800

    打包记录

--- markExternals.js
+++ markExternals.js
@@ -3,3 +3,5 @@
       vue,axios,vue-router,vuex 2019-02-11T06:07:44.155Z index.46100335.js
       vue,axios,vue-router,vuex Mon Feb 11 2019 14:18:44 GMT+0800 (GMT+08:00) index.46100335.js
       vue,axios,vue-router,vuex Mon Feb 11 2019 16:52:44 GMT+0800 (GMT+08:00) index.46100335.js
+      vue,axios,vue-router,vuex Thu Mar 07 2019 11:44:07 GMT+0800 (GMT+08:00) index.003d3b16.js
+      vue,axios,vue-router,vuex Thu Mar 07 2019 11:51:40 GMT+0800 (GMT+08:00) index.937041d6.js
